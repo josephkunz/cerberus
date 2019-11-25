271870b74ab5e75ec119379b0eb820dbfe22b5ba
Joseph Kunz <joseph.kunz@gmx.de>
2019-11-25 17:45:30 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cases, only: [ :index, :new, :create, :show ]
-  resources :clients, only: [ :create]
-  resources :infringements, only: [:create, :show]
-  resources :snapshots, only: [:index]
+  resources :infringements, only: [:create]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
