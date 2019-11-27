8ce7b85ada37ed80c69c7954fa47ed025f2a72f4
Joseph Kunz <joseph.kunz@gmx.de>
2019-11-27 13:21:24 +0100

    show action in infringement controller added

--- app/controllers/infringements_controller.rb
+++ app/controllers/infringements_controller.rb
@@ -3,6 +3,10 @@ class InfringementsController < ApplicationController
     @infringements = Infringement.all
   end
 
+  def show
+    @infringement = Infringement.find(params[:id])
+  end
+
   def create
     @infringement = Infringement.new(infringement_params)
     @case = Case.find(params[:case_id])
