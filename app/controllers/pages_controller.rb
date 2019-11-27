4829d55f050b01a5b0eeaa94738807718370de3b
Joseph Kunz <joseph.kunz@gmx.de>
2019-11-27 18:47:24 +0100

    change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    # merge
     if user_signed_in?
       redirect_to cases_path
     else
