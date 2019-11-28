ff998c0e9a0f3e097ece17a7757f9433c61def46
Ondrej Svejkovsky <ondrej.svejkovsky@gmail.com>
2019-11-28 14:59:48 +0100

    image processing fixed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,17 @@
 class Event < ApplicationRecord
-  validates :name, :frequency, :job_name, presence: true
+  belongs_to :infringement
+  validates :name, :frequency, :job_name, :infringement_id, presence: true
+
+  def if?(time)
+    case self.frequency
+    when 0
+      self.frequency = -1
+      self.save
+      return true
+    when -1
+      return false
+    else
+      return true
+    end
+  end
 end
