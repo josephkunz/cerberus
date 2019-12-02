c360c101945f385fcb673338955bd6a536699218
Reid Shea <reidshea3@gmail.com>
2019-12-02 12:27:03 +0100

    Navbar dropdown fix and case filtering by user implemented

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,6 +1,13 @@
 class CasesController < ApplicationController
+
   def index
-    @cases = Case.all
+    # If admin -> can see all cases, if normal user -> can see their own cases
+    if current_user.admin
+      @cases = Case.all
+    else
+      @cases = Case.where(user_id: current_user.id)
+    end
+
     @case = Case.new
     @client = Client.new
   end
@@ -19,7 +26,6 @@ class CasesController < ApplicationController
         @active_pages += 1
       end
     end
-
 
     # @infringement = Infringement.find(@case.infringements)
   end
