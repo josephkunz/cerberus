5ddf72fb3bb2402ea1aac8d0211f6cd1e093f6b9
Ondrej Svejkovsky <ondrej.svejkovsky@gmail.com>
2019-12-06 00:22:29 +0100

    cases page restyled completely

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,5 +2,7 @@ class Case < ApplicationRecord
   belongs_to :user
   belongs_to :client
   has_many :infringements, dependent: :destroy
+  has_many :events, through: :infringements
+  has_many :snapshots, through: :infringements
   accepts_nested_attributes_for :client
 end
